abd1e66d23e5dbd148572a2e348139fa8c1d139b
Algebra8 <mmnasrollahi@ucdavis.edu>
2019-07-08 20:03:29 -0700

    Created utils folder and file for helper functions

---
+++ EventsPipe/pipe/utils/utils.py
@@ -0,0 +1,145 @@
+import django
+import os, sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(BASE_DIR)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EventsPipe.settings')
+django.setup()
+
+
+# Django tools
+from django.http import JsonResponse
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+import json
+import datetime
+# Models
+from pipe.models import Event, Ticket
+
+
+
+
+
+def convert_string_to_timezone(date):
+    # Convert string to datetime based on %Y-%m-%d
+    date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    # Convert and return datetime to datetime with timezone
+    return timezone.make_aware(date)
+
+def events_list_to_dict(N_events, events_list):
+    events_dict = dict()
+    for i in range(N_events):
+        if i not in events_dict:
+            events_dict[i] = events_list[i]
+        else:
+            # Unique indices, won't get here
+            pass
+
+    return events_dict
+
+def get_events_list():
+    try:
+        events_dict = dict()
+        events = Event.objects.all()
+
+    except Event.DoesNotExist:
+        err = "Could not access Event records."
+        return JsonResponse({'error': err}, status=404)
+
+    else:
+        # Get descriptions of events
+        event_descr = [json.loads(e.description) for e in events]
+        # Convert list of events to dictionary
+        N_events = len(event_descr)
+        events_dict = events_list_to_dict(N_events, event_descr)
+
+        return JsonResponse(events_dict)
+
+def get_event_by_name(event_name):
+    try:
+        ev = Event.objects.get(name=event_name)
+
+    except Event.DoesNotExist:
+        return JsonResponse({
+            'event name': event_name,
+            'error': 'The given Event does not exist.'
+        }, status=404)
+
+    else:
+        data = json.loads(ev.description)
+        return JsonResponse(data)
+
+
+def get_event_by_id(eventid):
+    try:
+        ev = Event.objects.get(event_id=int(eventid))
+
+    except Event.DoesNotExist:
+        return JsonResponse({
+            'event id': eventid,
+            'error': 'The given Event does not exist.'
+        }, status=404)
+
+    else:
+        data = json.loads(ev.description)
+        return JsonResponse(data)
+
+
+def get_events_by_cost(cost):
+    try:
+        # Tickets contain one or more events
+        tickets = Ticket.objects.filter(ticket_cost=float(cost))
+
+    except Ticket.DoesNotExist:
+        return JsonResponse({
+            'ticket cost': cost,
+            'error': f"The given Event with cost {cost} does not exist."
+        }, status=404)
+
+    else:
+        events = [json.loads(t.event_id.description) for t in tickets]
+        # For multiple JSON responses, assign all required events to dict
+        N_events = len(tickets)  # number of events for given ticket cost
+        events_dict = events_list_to_dict(N_events, events)
+
+        return JsonResponse(events_dict)
+
+
+def get_by_startdate(utc_startdate):
+    """
+    `event` is a list containing all the different events for
+    any single start date.
+    """
+    try:
+        # utc_startdate must be in format %Y-%m-%d
+        parsed_sd = convert_string_to_timezone(utc_startdate)
+
+        events_by_sd = Event.objects.filter(
+            start_date__year=parsed_sd.year,
+            start_date__month=parsed_sd.month,
+            start_date__day=parsed_sd.day,
+        )
+
+    except Event.DoesNotExist:
+        # Proper date given, Event does not exist
+        return JsonResponse({
+            'event start date': utc_startdate,
+            'error': 'The given Event does not exist.'
+        }, status=404)
+
+    except ValueError:
+        # Improper date format given
+        return JsonResponse({
+            'event start date': utc_startdate,
+            'error': 'Bad Request',
+            'Message': "Format must be 'YYYY-MM-DD'."
+        }, status=400)
+
+    else:
+        events = [json.loads(e.description) for e in events_by_sd]
+
+        # Convert event descriptions to python dict
+        N_events = len(events)
+        events_dict = events_list_to_dict(N_events, events)
+
+        return JsonResponse(events_dict)
--- EventsPipe/pipe/views.py
+++ EventsPipe/pipe/views.py
@@ -12,6 +12,8 @@ import json
 import datetime
 # Models
 from pipe.models import Event, Ticket
+# Utils
+from pipe.utils import utils
 
 
 def index(request):
@@ -26,18 +28,18 @@ def get_event(request):
 
         if not q:
             # Return JSON of list of all Events
-            return get_events_list()
+            return utils.get_events_list()
 
         if len(q) > 1:
             return JsonResponse({'error': err_msg}, status=400)
 
         try:
             if 'event_name' in q:
-                return get_event_by_name(q['event_name'])
+                return utils.get_event_by_name(q['event_name'])
             elif 'start_date' in q:
-                return get_by_startdate(q['start_date'])
+                return utils.get_by_startdate(q['start_date'])
             elif 'ticket_cost' in q:
-                return get_events_by_cost(q['ticket_cost'])
+                return utils.get_events_by_cost(q['ticket_cost'])
 
         except KeyError:
             return JsonResponse({'error': err_msg}, status=400)
@@ -45,114 +47,6 @@ def get_event(request):
     return JsonResponse({
         'error': 'This endpoint is for GET requests only'
     }, status=404)
-
-
-def get_events_list():
-    try:
-        events_dict = dict()
-        events = Event.objects.all()
-
-    except Event.DoesNotExist:
-        err = "Could not access Event records."
-        return JsonResponse({'error': err}, status=404)
-
-    else:
-        # Get descriptions of events
-        event_descr = [json.loads(e.description) for e in events]
-        # Convert list of events to dictionary
-        N_events = len(event_descr)
-        events_dict = events_list_to_dict(N_events, event_descr)
-
-        return JsonResponse(events_dict)
-
-def get_event_by_name(event_name):
-    try:
-        ev = Event.objects.get(name=event_name)
-
-    except Event.DoesNotExist:
-        return JsonResponse({
-            'event name': event_name,
-            'error': 'The given Event does not exist.'
-        }, status=404)
-
-    else:
-        data = json.loads(ev.description)
-        return JsonResponse(data)
-
-
-def get_event_by_id(eventid):
-    try:
-        ev = Event.objects.get(event_id=int(eventid))
-
-    except Event.DoesNotExist:
-        return JsonResponse({
-            'event id': eventid,
-            'error': 'The given Event does not exist.'
-        }, status=404)
-
-    else:
-        data = json.loads(ev.description)
-        return JsonResponse(data)
-
-
-def get_events_by_cost(cost):
-    try:
-        # Tickets contain one or more events
-        tickets = Ticket.objects.filter(ticket_cost=float(cost))
-
-    except Ticket.DoesNotExist:
-        return JsonResponse({
-            'ticket cost': cost,
-            'error': f"The given Event with cost {cost} does not exist."
-        }, status=404)
-
-    else:
-        events = [json.loads(t.event_id.description) for t in tickets]
-        # For multiple JSON responses, assign all required events to dict
-        N_events = len(tickets)  # number of events for given ticket cost
-        events_dict = events_list_to_dict(N_events, events)
-
-        return JsonResponse(events_dict)
-
-
-def get_by_startdate(utc_startdate):
-    """
-    `event` is a list containing all the different events for
-    any single start date.
-    """
-    try:
-        # utc_startdate must be in format %Y-%m-%d
-        parsed_sd = convert_string_to_timezone(utc_startdate)
-
-        events_by_sd = Event.objects.filter(
-            start_date__year=parsed_sd.year,
-            start_date__month=parsed_sd.month,
-            start_date__day=parsed_sd.day,
-        )
-
-    except Event.DoesNotExist:
-        # Proper date given, Event does not exist
-        return JsonResponse({
-            'event start date': utc_startdate,
-            'error': 'The given Event does not exist.'
-        }, status=404)
-
-    except ValueError:
-        # Improper date format given
-        return JsonResponse({
-            'event start date': utc_startdate,
-            'error': 'Bad Request',
-            'Message': "Format must be 'YYYY-MM-DD'."
-        }, status=400)
-
-    else:
-        events = [json.loads(e.description) for e in events_by_sd]
-
-        # Convert event descriptions to python dict
-        N_events = len(events)
-        events_dict = events_list_to_dict(N_events, events)
-
-        return JsonResponse(events_dict)
 
 
 @csrf_exempt
@@ -168,7 +62,7 @@ def update_event(request, eventid):
             body = json.loads(request.body)
 
             # Check if JSON request is valid
-            not_valid = request_validation(body, event_dict)
+            not_valid = utils.request_validation(body, event_dict)
             if not_valid:
                 return not_valid
 
@@ -179,7 +73,7 @@ def update_event(request, eventid):
         # Update fields of Event object
         event.name = event_dict['name']
         event.event_id = event_dict['id']
-        event.start_date = convert_string_to_timezone(event_dict['start']['utc'])
+        event.start_date = utils.convert_string_to_timezone(event_dict['start']['utc'])
 
         # Convert object back to JSON and place in event
         event.description = json.dumps(event_dict)
@@ -206,21 +100,3 @@ def update_event(request, eventid):
         'Error': "I'm a teapot",
         'message': msg,
     }, status=418)
-
-
-def convert_string_to_timezone(date):
-    # Convert string to datetime based on %Y-%m-%d
-    date = datetime.datetime.strptime(date, '%Y-%m-%d')
-    # Convert and return datetime to datetime with timezone
-    return timezone.make_aware(date)
-
-def events_list_to_dict(N_events, events_list):
-    events_dict = dict()
-    for i in range(N_events):
-        if i not in events_dict:
-            events_dict[i] = events_list[i]
-        else:
-            # Unique indices, won't get here
-            pass
-
-    return events_dict
